ae0cb05464f0f9c398fcbbbfb1ed84e4764e3a0e
quads08 <aminbkl99@gmail.com>
2020-10-25 13:30:18 +0100

    your message

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CaptionComponent } from './caption/caption.component';
+import { SearchComponent } from './search/search.component';
+import { ListingsComponent } from './listings/listings.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CaptionComponent,
+    SearchComponent,
+    ListingsComponent,
+    FooterComponent,
+    HeroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
